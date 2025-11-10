f1b06d5432c57e7ded766c9db9c37e2ed38e6b37
EAbraham31 <B316B312@gmail.com>
2025-11-09 19:48:49 -0600

    Agregar relaciones completas en todos los modelos

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['name','category_id','user_id','cost','stock'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function qrcode()
+    {
+        return $this->hasOne(Qrcode::class);
+    }
+    
+    public function movements()
+    {
+        return $this->hasMany(StockMovement::class);
+    }
 }
