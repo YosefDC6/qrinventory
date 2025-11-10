d149adb27285193a8626de9552057967a3a25062
EAbraham31 <B316B312@gmail.com>
2025-11-09 21:51:42 -0600

    Agregar relaciones en models Qrcode y StockMovement

--- app/Models/Qrcode.php
+++ app/Models/Qrcode.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Qrcode extends Model
 {
-    //
+    protected $fillable = ['product_id','code'];
+    
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
--- app/Models/StockMovement.php
+++ app/Models/StockMovement.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class StockMovement extends Model
 {
-    //
+    protected $fillable = ['product_id','type','quantity','note'];
+    
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
